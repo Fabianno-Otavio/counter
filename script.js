b9c36b3b653b5032ff157ff4f95d091745f03e74
Fabianno Otávio <fabiannootavio08@gmail.com>
2022-07-19 18:10:26 -0300

    Fix: fixed step interval

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function contar(){
     var passo = document.getElementById('passo')
     var res = document.getElementById('res')
     res.innerHTML=''
-    if(passo.value == 0){
-        res.innerHTML='Digite o passo.'
+    if(passo.value <= 0){
+        res.innerHTML='Invalid step.'
     } else if(inicio.value == '' || fim.value == ''){
-        res.innerHTML='Impossível contar. Digite algum valor.'
+        res.innerHTML='Impossible to count.'
     } else {
     if(inicio.value>fim.value){
         for(var i = Number(inicio.value);i>=Number(fim.value);i-=Number(passo.value)){
