6671f4abe0a207f9a543927b8004c840459c06b7
Fabianno Otávio <fabiannootavio08@gmail.com>
2022-07-20 16:54:29 -0300

    Fix: change style

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function contar(){
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
+    var start = document.getElementById('start')
+    var end = document.getElementById('end')
+    var step = document.getElementById('step')
     var res = document.getElementById('res')
     res.innerHTML=''
-    if(passo.value <= 0){
+    if(step.value <= 0){
         res.innerHTML='Invalid step.'
-    } else if(inicio.value == '' || fim.value == ''){
+    } else if(start.value == '' || end.value == ''){
         res.innerHTML='Impossible to count.'
     } else {
-    if(inicio.value>fim.value){
-        for(var i = Number(inicio.value);i>=Number(fim.value);i-=Number(passo.value)){
+    if(start.value>end.value){
+        for(var i = Number(start.value);i>=Number(end.value);i-=Number(step.value)){
         res.innerHTML+= ` ${i} \u{1F449}`
         }
     } else {
-    for(var i = Number(inicio.value);i<=Number(fim.value);i+=Number(passo.value)){
+    for(var i = Number(start.value);i<=Number(end.value);i+=Number(step.value)){
         res.innerHTML+= ` ${i} \u{1F449}`
     }
     }
